2cbf890c5c01fdfc1974c93d385c2637d2383f1a
Lance Boylan <eg01st3220@gmail.com>
2026-03-25 15:41:28 -0600

    Finished

--- o16/Program.cs
+++ o16/Program.cs
@@ -1,7 +1,18 @@
-﻿internal class Program
+﻿using o16;
+internal class Program
 {
     private static void Main(string[] args)
     {
-        
+        Truck truck = new Truck();
+        truck.StartEngine();
+        truck.LoadCargo();
+
+        Car car = new Car();
+        car.StartEngine();
+        car.PlayRadio();
+
+        Motorcycle motorcycle = new Motorcycle();
+        motorcycle.StartEngine();
+        motorcycle.DoWheelie();
     }
 }
